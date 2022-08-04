ca25b23642a2a2600fdedc6f022d03a3b495618a
kevinbowe <kevinbowe1957@gmail.com>
2022-08-04 11:10:28 -0400

    Add project files.

---
+++ Controller.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Injection_SMTP_Demo_master_VS2022
+{
+	public struct ParsedName
+	{
+		public string First { get; set; }
+		public string Last { get; set; }
+	}
+
+	public class Controller
+	{
+		const string theMessage = "Lets get together, play some music and have some fun";
+		//public readonly char[] nameTokens = new char[] { ' ', ',' };
+
+		/// <summary>
+		/// Construct the email message body
+		/// </summary>
+		/// <param name="name">Pre-parsed name</param>
+		/// <param name="parsedName">Parsed name struct: First: Last:</param>
+		/// <param name="nameParser">NameParser object</param>
+		/// <param name="emailSender">EmailSender object</param>
+		/// <returns>String</returns>
+		public static string BuildMessage(string name, ParsedName parsedName, INameParser nameParser, IEmailSender emailSender)
+		{
+			string parserName = nameParser.GetType().ToString();
+			string emailSenderName = emailSender.GetType().ToString();
+
+			var message =
+				 $"{theMessage}\n\n" +
+				 $"------------\n" +
+				 $"Original Input Name: {name}\n" +
+				 $"First: {parsedName.First}\n" +
+				 $"Last: {parsedName.Last} \n" +
+				 $"Name Parsed With: {parserName} \n" +
+				 $"Message Sent With: {emailSenderName}" +
+				 $"\n\n\n";
+			return message;
+		} // END_BuildMessage
+
+		/// <summary>
+		/// This returns the proper form of the name
+		/// </summary>
+		/// <param name="name">Pre-parsed name</param>
+		/// <example>Kevin Bowe will return "FirstLast". Bowe, Kevin will return "LastFirst"</example>
+		/// <returns>String</returns>
+		public static string GetNameForm(string name)
+		{
+			if (name.IndexOfAny(new char[] { ' ', ',' }) < 0) return "OneName";
+
+			if (name.IndexOf(',') > 0)
+				return "LastFirst";
+			return "FirstLast";
+		}
+	} //-- END Controller Class
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	public interface INameParser
+	{
+		ParsedName ParseName(string input);
+	}
+
+	public class OneNameParser : INameParser
+	{
+		public ParsedName ParseName(string input)
+		{
+
+			return new ParsedName() { First = input };
+
+		} // END_ParseName
+
+	} // END_OneNameParser_Class
+
+	public class FirstLastParser : INameParser
+	{
+		public ParsedName ParseName(string input)
+		{
+			string[] sa = input.Split();
+			return new ParsedName()
+			{
+				First = sa[0].Trim(),
+				Last = sa[1].Trim()
+			};
+
+		} // END_ParseName
+
+	} // END_FirstLastParser_Class
+
+	public class LastFirstParser : INameParser
+	{
+		public ParsedName ParseName(string input)
+		{
+			string[] sa = input.Split(',');
+			return new ParsedName()
+			{
+				First = sa[1].Trim(),
+				Last = sa[0].Trim()
+			};
+		} // END_ParseName
+
+	} // END_LastFirstParser_Class
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	public interface IEmailSender
+	{
+		void SendEmail(string to, string from, string subject, string body);
+	}
+
+	public class MyEmailSender : IEmailSender
+	{
+		public void SendEmail(string to, string from, string subject, string body)
+		{
+			string message = string.Format(
+				 "eMail Sent\n" +
+				 "----------\n" +
+				 "To: {0}\n" +
+				 "From: {1} \n" +
+				 "Subject: {2}\n\n" +
+				 "{3}",
+				 to, from, subject, body
+			);
+
+			Console.WriteLine(message);
+		}
+
+	} // END_MyEmailSender_Class
+
+	public class YourEmailSender : IEmailSender
+	{
+		public void SendEmail(string to, string from, string subject, string body)
+		{
+			string message = string.Format(
+				 "eMail Sent\n" +
+				 "----------\n" +
+				 "To: {0}\n" +
+				 "From: {1}\n" +
+				 "Subject: {2}\n\n" +
+				 "{3}",
+				 to, from, subject, body
+			);
+
+			Console.WriteLine(message);
+		}
+	} // END_YourEmailSender_Class
+
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using Dependency_Injection_SMTP_Demo_master_VS2022;
+
+//-- No instance of Controller is required -- All references are static
+//-- Controller controller = new Controller();
+
+List<string> nameList = new List<string>()
+				{
+					 "kevin bowe",
+					 "bowe, kevin",
+					 "Johnney Cash",
+					 "Miles Davis",
+					 "Plant, Robert",
+					 "Gaga, Lady",
+					 "Lin-Manual Miranda",
+					 "Bono"
+				};
+
+
+// Inject the correct dependency based on the name format.
+foreach (string name in nameList)
+{
+	// choose nameParser
+	INameParser nameParser;
+	switch (Controller.GetNameForm(name))
+	{
+		case "FirstLast":
+			nameParser = new FirstLastParser();
+			break;
+		case "LastFirst":
+			nameParser = new LastFirstParser();
+			break;
+		case "OneName":
+		default:
+			nameParser = new OneNameParser();
+			break;
+	}
+	ParsedName parsedName = nameParser.ParseName(name);
+
+	// choose EmailSender
+	IEmailSender emailSender;
+	if (parsedName.Last?.Length == 4)
+			emailSender = new MyEmailSender();
+	else emailSender = new YourEmailSender();
+
+	// Build the message
+	string message = Controller.BuildMessage(name, parsedName, nameParser, emailSender);
+
+	// Send the message
+	emailSender.SendEmail("joe@gmail.com", "me@hotmail.com", "Let play some music", message);
+
+} // END_FOREACH
+
+
+
+
+
+
